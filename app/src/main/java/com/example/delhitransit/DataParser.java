fd0ce9e7bc8c9369d96087796cc8c78b4c75b5c4
Tanmay Singal <tanmaysingal@icloud.com>
2019-06-11 23:44:36 +0530

    Removed Retrofit2. Download protobuf using HttpUrlConnection

--- app/src/main/java/com/example/delhitransit/DataParser.java
+++ app/src/main/java/com/example/delhitransit/DataParser.java
@@ -2,57 +2,55 @@ package com.example.delhitransit;
 
 import android.util.Log;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
-
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class DataParser {
 
     private static final String LOG_TAG = DataParser.class.getSimpleName();
 
-    public static final String BASE_URL = "https://otd.delhi.gov.in/api/realtime/";
-    private static final String API_KEY = "ObnMpq02enRlNc8m2iuOnR97GLnfKQUj";
-
-    private static boolean isLoaded = false;
-    private static List<GtfsRealtime.FeedEntity> feedEntity;
-
     public static List<GtfsRealtime.FeedEntity> fetchPositionData() {
 
-        makeRetrofitRequest();
 
-        while (!isLoaded) {
-            Log.d(LOG_TAG, "BEFORE LOADER");
+        List<GtfsRealtime.FeedEntity> feedEntityList = null;
+        String urlString = "https://otd.delhi.gov.in/api/realtime/VehiclePositions.pb?key=ObnMpq02enRlNc8m2iuOnR97GLnfKQUj";
+
+
+        try {
+
+            URL url = new URL(urlString);
+            InputStream inputStream = null;
+
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            if (urlConnection.getResponseCode() == 200) {
+
+                Log.d(LOG_TAG, "Connected to server " + urlConnection.getResponseMessage());
+
+                inputStream = urlConnection.getInputStream();
+
+                GtfsRealtime.FeedMessage feedMessage = GtfsRealtime.FeedMessage.parseFrom(inputStream);
+                feedEntityList = feedMessage.getEntityList();
+
+                Log.d(LOG_TAG, "List Size : " + feedEntityList.size());
+
+            } else
+                Log.e(LOG_TAG, "Error connecting to server. Response code : " + urlConnection.getResponseCode());
+
+
+        } catch (
+                Exception e) {
+            e.printStackTrace();
         }
 
-        return feedEntity;
+        return feedEntityList;
 
     }
-
-    private static void makeRetrofitRequest() {
-        Call<ResponseBody> call = RetrofitClient
-                .getInstance()
-                .getAPIClient()
-                .getVehiclePositions(API_KEY);
-
-        call.enqueue(new Callback<ResponseBody>() {
-            @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-
-                Log.d(LOG_TAG, "Success! " + response.message());
-                isLoaded = true;
-
-            }
-
-            @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                Log.d(LOG_TAG, "Retrofit Call Failed!");
-                isLoaded = true;
-            }
-        });
-    }
-
 
 }
--- app/src/main/java/com/example/delhitransit/MainActivity.java
+++ app/src/main/java/com/example/delhitransit/MainActivity.java
@@ -3,21 +3,26 @@ package com.example.delhitransit;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.loader.content.AsyncTaskLoader;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.LoaderManager;
+import android.content.AsyncTaskLoader;
 import android.content.Context;
+import android.content.Loader;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<GtfsRealtime.FeedEntity>> {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private static BusListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,13 +54,87 @@ public class MainActivity extends AppCompatActivity {
 //
 //        }
 
-        VehiclePositionLoader loader = new VehiclePositionLoader(this);
-        loader.loadInBackground();
+
+        RecyclerView busListView = findViewById(R.id.bus_list);
+        busListView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new BusListAdapter(null);
+        busListView.setAdapter(adapter);
+
+        getLoaderManager().initLoader(2924, null, this);
+
+    }
+
+    @Override
+    public Loader<List<GtfsRealtime.FeedEntity>> onCreateLoader(int i, Bundle bundle) {
+        return new VehiclePositionLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<GtfsRealtime.FeedEntity>> loader, List<GtfsRealtime.FeedEntity> feedEntities) {
+        Log.d(LOG_TAG, "List Size Recieved by Loader : " + feedEntities.size());
+        adapter.mDataset = feedEntities;
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<GtfsRealtime.FeedEntity>> loader) {
+        adapter.mDataset = null;
+        adapter.notifyDataSetChanged();
+    }
+
+    private class BusListAdapter extends RecyclerView.Adapter<BusListAdapter.MyViewHolder> {
+
+
+        private List<GtfsRealtime.FeedEntity> mDataset;
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public TextView textView;
+
+            public MyViewHolder(TextView v) {
+                super(v);
+                textView = v;
+            }
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public BusListAdapter(List<GtfsRealtime.FeedEntity> myDataset) {
+            mDataset = myDataset;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public BusListAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            // create a new view
+            TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.list_item_bus, parent, false);
+
+            MyViewHolder vh = new MyViewHolder(v);
+            return vh;
+        }
+
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.textView.setText(mDataset.get(position).getId());
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            if (mDataset != null) {
+                return mDataset.size();
+            }
+            return 0;
+        }
 
     }
 
 
-    private class VehiclePositionLoader extends AsyncTaskLoader<List<GtfsRealtime.FeedEntity>> {
+    private static class VehiclePositionLoader extends AsyncTaskLoader<List<GtfsRealtime.FeedEntity>> {
 
 
         private String LOG_TAG = VehiclePositionLoader.class.getSimpleName();
@@ -73,7 +152,14 @@ public class MainActivity extends AppCompatActivity {
         @Nullable
         @Override
         public List<GtfsRealtime.FeedEntity> loadInBackground() {
-            return DataParser.fetchPositionData();
+            Log.d(LOG_TAG, "Begin Loading");
+            List<GtfsRealtime.FeedEntity> entityList = DataParser.fetchPositionData();
+
+            for (GtfsRealtime.FeedEntity entity : entityList) {
+                Log.v(LOG_TAG, entity.getId() + entity.getVehicle().getStopId());
+            }
+
+            return entityList;
         }
     }
 
